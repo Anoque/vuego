f89d06b8ad84d9ec3b52c98677496c536baeaa91
Ayas <anoque@mail.ru>
2019-07-11 11:38:35 +0700

    Update users controller

--- back/controllers/users.go
+++ back/controllers/users.go
@@ -11,10 +11,12 @@ func CreateUser(req *http.Request) {
 	username = req.FormValue("username")
 	password = req.FormValue("password")
 
-	if len(username) > 0 && len(password) > 0 {
+	log.Println(username, password)
+
+	/*if len(username) > 0 && len(password) > 0 {
 		log.Println("Yes")
 	} else {
 		panic("Not all parameters")
-	}
+	}*/
 
 }
--- back/main.go
+++ back/main.go
@@ -3,19 +3,30 @@ package main
 import (
 	"./controllers"
 	"github.com/go-martini/martini"
+	"github.com/martini-contrib/cors"
 	"net/http"
 )
 
 func main() {
 	m := martini.Classic()
 
+	m.Use(cors.Allow(&cors.Options{
+		AllowOrigins:     []string{"*"},
+		AllowMethods:     []string{"POST", "GET", "DELETE", "PUT", "PATCH"},
+		AllowHeaders:     []string{"Origin"},
+	}))
+
 	m.Get("/", func() string {
 		return controllers.GetTests()
 	})
 
 	m.Group("/users", func(r martini.Router) {
-		r.Post("/add", func(params martini.Params, req *http.Request) {
+		r.Post("/add", func(params martini.Params, req *http.Request, res http.ResponseWriter) string {
 			controllers.CreateUser(req)
+
+			res.WriteHeader(200)
+
+			return "OK"
 		})
 	})
 
